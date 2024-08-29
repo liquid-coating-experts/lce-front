91937ebe8c303401185fd659e0cdae2e8c9d90fa
Stuart Bloxham <s.bloxham@hotmail.co.uk>
2024-08-29 19:40:15 +0100

    changed build and export commands to output: export

--- next.config.js
+++ next.config.js
@@ -14,4 +14,4 @@ module.exports = withStoreConfig({
   },
 })
 
-console.log("next.config.js", JSON.stringify(module.exports, null, 2))
+// console.log("next.config.js", JSON.stringify(module.exports, null, 2))
